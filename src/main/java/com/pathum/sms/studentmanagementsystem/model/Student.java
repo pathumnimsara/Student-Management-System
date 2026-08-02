4f4fbc9a4dc7874df79c1bff1c8a70d5b252e804
PathumNimsara <pathumnimesh253@gmail.com>
2026-08-02 16:45:56 +0530

    Add Student model constructors and methods

--- src/main/java/com/pathum/sms/studentmanagementsystem/model/Student.java
+++ src/main/java/com/pathum/sms/studentmanagementsystem/model/Student.java
@@ -7,4 +7,60 @@ public class Student {
     private int age;
     private String email;
 
+    // Default Constructor
+    public Student() {
+    }
+
+    // Parameterized Constructor
+    public Student(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Display object details
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
