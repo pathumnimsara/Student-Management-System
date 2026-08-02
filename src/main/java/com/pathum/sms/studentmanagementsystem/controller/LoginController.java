9f5812254b6296d8f121cd649415195f7bcafe03
PathumNimsara <pathumnimesh253@gmail.com>
2026-08-02 16:50:20 +0530

    Setup login view and controller

---
+++ src/main/java/com/pathum/sms/studentmanagementsystem/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.pathum.sms.studentmanagementsystem.controller;
+
+public class LoginController {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.pathum.sms.studentmanagementsystem {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.pathum.sms.studentmanagementsystem to javafx.fxml;
+    opens com.pathum.sms.studentmanagementsystem.controller to javafx.fxml;
+
     exports com.pathum.sms.studentmanagementsystem;
+    exports com.pathum.sms.studentmanagementsystem.controller;
 }
