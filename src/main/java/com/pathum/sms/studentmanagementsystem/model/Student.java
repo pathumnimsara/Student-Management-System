5ed379ae51043c9781eaa02dd2649008644e49a8
PathumNimsara <pathumnimesh253@gmail.com>
2026-08-02 16:42:47 +0530

    Create project package structure and Student model

---
+++ src/main/java/com/pathum/sms/studentmanagementsystem/model/Student.java
@@ -0,0 +1,10 @@
+package com.pathum.sms.studentmanagementsystem.model;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+
+}
